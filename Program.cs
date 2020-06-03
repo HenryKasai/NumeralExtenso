2281fd892ce5f2aa910b7dce1ac93dd2c879229f
HenryKasai <henrykasai1775@gmail.com>
2020-06-03 18:20:53 -0300

    Exercício Resolvido.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Humanizer;
+
+namespace NumeralExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Insira um número inteiro.");
+            int número = Convert.ToInt32(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Você digitou o número {número.ToWords()}.");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+        }
+    }
+}
